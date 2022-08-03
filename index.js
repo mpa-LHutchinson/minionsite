29a3845d616318e75b6b0f2ed4cf9995471721ea
mpa-LHutchinson <49694715+mpa-LHutchinson@users.noreply.github.com>
2022-08-03 18:40:22 -0400

    Add files via upload
    added files

---
+++ index.js
@@ -0,0 +1,5 @@
+function playBananaSong(){
+    document.getElementById("bananasong").play();
+    document.getElementById("minion").classList.add("header");
+    document.getElementById("minion").innerHTML = "Ba-ba-ba-ba-ba-nana";
+}
